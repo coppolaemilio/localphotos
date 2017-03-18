2562db85e65f52b7f7dd298fdb263858ddfd3798
Emilio Coppola <coppolaemilio@gmail.com>
2017-03-18 19:13:08 +0100

    Adding more photos and next/previous buttons

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 app.debug = True
 
-photos_path = './static/photos/'
-
-photos = os.listdir(photos_path)
+file_list = os.listdir(app.static_folder)
 current_index = 0
-current_photo = photos[current_index]
-
-
+current_file = file_list[current_index]
 
 @app.route('/')
 def index():
-    return render_template('main.html', photo_url=current_photo)
+    return render_template('main.html', file_url=current_file, current_index = current_index)
 
 @app.route('/next')
 def next():
-    global photos
+    global file_list
     global current_index
-    global current_photo
+    global current_file
     current_index += 1
-    if current_index > len(photos) - 1:
+    if current_index > len(file_list) - 1:
         current_index = 0
-    current_photo = photos[current_index]
-    return render_template('main.html', photo_url=current_photo)
+    current_file = file_list[current_index]
+    return redirect(url_for('index'))
 
+@app.route('/previous')
+def previous():
+    global file_list
+    global current_index
+    global current_file
+    
+    if current_index - 1 < 0:
+        current_index = len(file_list) -1
+    else:
+        current_index -= 1
+    current_file = file_list[current_index]
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=True)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# LocalPhotos
+Start a pyhon server on a folder to share with all the local devices a gallery of images or videos.
